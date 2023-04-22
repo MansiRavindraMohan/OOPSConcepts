8f1f4acb12b57bc035c27f1bc1ff19bf12954530
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-22 17:57:01 +0530

    Print the name

---
+++ OOPSConcepts/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OOPSConcepts
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ClassName name= new ClassName();
+            name.PrintName();
+        }
+    }
+}
