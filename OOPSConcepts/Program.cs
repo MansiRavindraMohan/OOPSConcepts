7fd140938761fd1f7fab8f5f511581e30abffb14
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-27 15:54:49 +0530

    Example of polymorphism-overloading

--- OOPSConcepts/Program.cs
+++ OOPSConcepts/Program.cs
@@ -6,17 +6,12 @@ namespace OOPSConcepts
     {
         public static void Main(string[] args)
         {
-            Encapsulation encap = new Encapsulation();
-            Console.WriteLine(encap.Name);
-            encap.Name = "MANSI";
-            Console.WriteLine(encap.Name);
-            Console.WriteLine(encap.RollNo = 456);
-            //OverLoading over =new OverLoading();
-            //over.MethodOne();
-            //over.MethodOne(4,"Mansi");
-            //over.MethodOne(6,"mansi");
+            //Polymorphism
+            OverLoading over = new OverLoading();
+            over.MethodOne();
+            over.MethodOne(4, "Mansi");
+            over.MethodOne(6, "mansi");
             Console.ReadKey();
-
         }
     }
 }
