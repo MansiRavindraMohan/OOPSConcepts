ef337c1b815be69e65ce564969da572e42627f3a
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-27 15:40:10 +0530

    Examples of reference type, value type and type casting

--- OOPSConcepts/Program.cs
+++ OOPSConcepts/Program.cs
@@ -6,12 +6,20 @@ namespace OOPSConcepts
     {
         public static void Main(string[] args)
         {
-
-            Encapsulation encap = new Encapsulation();
-            Console.WriteLine(encap.Name);
-            encap.Name = "MANSI";
-            Console.WriteLine(encap.Name);
-            Console.WriteLine(encap.RollNo = 456);
+            TypeCasting obj = new TypeCasting();
+            obj.Method();
+            ValueType type = new ValueType();
+            type.MethodValueType();
+            ReferenceType typeOne = new ReferenceType();
+            int value = 5;
+            Console.WriteLine(value);
+            typeOne.MethodReferenceType(ref value);
+            Console.WriteLine(value);
+            //Encapsulation encap=new Encapsulation();
+            //Console.WriteLine(encap.Name);
+            //encap.Name = "MANSI";
+            //Console.WriteLine(encap.Name);
+            //Console.WriteLine(encap.RollNo=456);
             //OverLoading over =new OverLoading();
             //over.MethodOne();
             //over.MethodOne(4,"Mansi");
