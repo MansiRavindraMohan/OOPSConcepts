b7927bcf4943ab17b850ba785fd6702c5729b38a
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-27 15:32:09 +0530

    Examples of reference type, value type and type casting

---
+++ OOPSConcepts/ReferenceType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcepts
+{
+    public class ReferenceType
+    {
+        public void MethodReferenceType(ref int num)
+        {
+            num = num * num;
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ OOPSConcepts/TypeCasting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcepts
+{
+    //Typecasting is used to assign a value of data type to another datatype.
+    //There are two types of casting implicit and explicit.
+    //Implicit:It means automatically converting a small type to a large type.
+    //char=> int=> long=> float=> double
+    //Explicit:It should be manually.
+    //double=> float=> long=> int=> char
+    
+    public class TypeCasting
+    {
+        public void Method()
+        {
+            int value = 10;
+            double valueOne = value;
+            int valueTwo = 20;
+            //long add=valueOne+valueTwo;
+            Console.WriteLine(value);
+            Console.WriteLine(valueOne);
+            double valueThree = 10.5;
+            int valueFour = (int)valueThree;
+            Console.WriteLine(valueFour);
+        }
+    }
+}
