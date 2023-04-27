7bbb746a0069ee24fa09a2dbb4ed5f31c2680ab4
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-27 15:43:40 +0530

    Example of encapsulation

--- OOPSConcepts/Program.cs
+++ OOPSConcepts/Program.cs
@@ -6,20 +6,11 @@ namespace OOPSConcepts
     {
         public static void Main(string[] args)
         {
-            TypeCasting obj = new TypeCasting();
-            obj.Method();
-            ValueType type = new ValueType();
-            type.MethodValueType();
-            ReferenceType typeOne = new ReferenceType();
-            int value = 5;
-            Console.WriteLine(value);
-            typeOne.MethodReferenceType(ref value);
-            Console.WriteLine(value);
-            //Encapsulation encap=new Encapsulation();
-            //Console.WriteLine(encap.Name);
-            //encap.Name = "MANSI";
-            //Console.WriteLine(encap.Name);
-            //Console.WriteLine(encap.RollNo=456);
+            Encapsulation encap = new Encapsulation();
+            Console.WriteLine(encap.Name);
+            encap.Name = "MANSI";
+            Console.WriteLine(encap.Name);
+            Console.WriteLine(encap.RollNo = 456);
             //OverLoading over =new OverLoading();
             //over.MethodOne();
             //over.MethodOne(4,"Mansi");
